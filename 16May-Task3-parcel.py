ec86d6c48db0e2a0868872267e63da332531b246
Paul Satish <paulsatish@node-001-114.fwcs.k12.in.us>
2017-06-27 14:10:26 +0800

    new

---
+++ 16May-Task3-parcel.py
@@ -0,0 +1,45 @@
+#1
+TotalNotAccepted=0
+TotalAccepted=0
+TotalWeight=0
+
+price=0
+maxpay=0
+continue1="yes"
+while continue1=="yes":
+    maxweight=10
+    print("Please enter the parcel dimensions")
+    height=float(input("Please enter the height of the parcel in cm: "))
+    width=float(input("Please enter the width of the parcel in cm: "))
+    length=float(input("Please enter the length of the parcel in cm: "))
+
+    dimSum=height+width+length
+
+    if height>80 or width>80 or length>80:
+        print("One of the dimensions are more than 80cm.  Please enter <=80")
+
+    if dimSum>200:
+        print("Sum of three dimensions should be <= 200")
+
+    weight=float(input("Please enter the weight of the parcel in KG"))
+    if weight<1 or weight >12 or height>80 or width>80 or length>80 or dimSum>200:
+        print("Parcel rejected")
+        TotalNotAccepted=TotalNotAccepted+1
+    elif weight>=1 and weight<=5:
+        price=10
+        print("please pay $"+ str(price))
+    elif weight>5 and weight<=10:
+        maxweight=weight-5
+        maxweightInGrams=maxweight*1000
+        maxpay=1000+(maxweightInGrams*0.10)
+        weightingrams=maxpay/100
+        print("Please pay $" +str(weightingrams))
+        TotalWeight=TotalWeight+weight
+        TotalAccepted=TotalAccepted+1
+    print("Total number of parcels accepted are :" + str(TotalAccepted))
+    print("Total number of parcels not accepted are :" + str(TotalNotAccepted))  
+    print("Total weight of all the accepted parcels is:" +str(TotalWeight))
+    continue1=input("Do you want to continue?")
+             
+
+ 
